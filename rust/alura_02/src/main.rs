c0e6f4f86c1fc95a7172a968bc4d7b9044c90413
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-07-12 11:22:48 -0300

    Applying enums

--- rust/alura_02/src/main.rs
+++ rust/alura_02/src/main.rs
@@ -1,19 +1,59 @@
 fn main() {
-    let notas: [f32; 4] = [10.0, 8.0, 7.0, 9.0];
 
-    for nota in notas {
-    println!("Nota: {}", nota);
+    // matriz();
+
+    // println!("Fim de semana? {}", eh_fim_de_semana_2(DiaDaSemana::Domingo));
+
+    let cor: Cor = Cor::CymkColor{cyan: 25, yellow: 25, magenta: 25, black: 255};
+
+    println!("A cor é {}.", cores(cor))
+}
+
+#[allow(dead_code)]
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
+
+fn eh_fim_de_semana(dia: u8) -> bool {
+    match dia {
+        0 | 6 => true,
+        _ => false
     }
+}
 
-    println!();
-
-    for i in 0..notas.len() {
-        println!("Nota {}: {}", i, notas[i]);
+fn eh_fim_de_semana_2(dia: DiaDaSemana) -> bool {
+    match dia {
+        DiaDaSemana::Domingo | DiaDaSemana::Sabado => true,
+        _ => false
     }
+}
 
-    println!();
+#[allow(dead_code)]
+enum Cor {
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8, u8, u8),
+    CymkColor{cyan: u8, yellow: u8, magenta: u8, black: u8}
+}
 
-    matriz();
+fn cores(cor: Cor) -> String {
+    match cor {
+        Cor::Red | Cor::RgbColor(255, 0, 0) => String::from("vermelho"),
+        Cor::Green | Cor::RgbColor(0, 255, 0) => String::from("verde"),
+        Cor::Blue | Cor::RgbColor(0, 0, 255) => String::from("azul"),
+        Cor::RgbColor(0, 0, 0)
+            | Cor::CymkColor{cyan: _, yellow: _, magenta: _, black: 255} => String::from("preto"),
+        Cor::RgbColor(255, 255, 255) => String::from("branco"),
+        Cor::RgbColor(_, _, _) => String::from("RGB desconhecido"),
+        Cor::CymkColor{cyan: _, yellow: _, magenta: _, black: _} => String::from("CYMK desconhecido")
+    }
 }
 
 fn matriz() {
@@ -26,5 +66,39 @@ fn matriz() {
         for j in 0..3 {
             println!("M[{i},{j}] = {}", m[i][j])
         }
+    }let notas: [f32; 4] = [10.0, 8.0, 7.0, 9.0];
+    let inteiro: usize = 2;
+
+    for nota in notas {
+    println!("Nota: {}", nota);
     }
+
+    println!();
+
+    for i in 0..notas.len() {
+        println!("Nota {}: {}", i, notas[i]);
+    }
+
+    println!();
+    println!("Nota {}: {}", inteiro, notas[inteiro]);
+    println!();
+}
+
+fn lista_de_notas() {
+    let notas: [f32; 4] = [10.0, 8.0, 7.0, 9.0];
+    let inteiro: usize = 2;
+
+    for nota in notas {
+    println!("Nota: {}", nota);
+    }
+
+    println!();
+
+    for i in 0..notas.len() {
+        println!("Nota {}: {}", i, notas[i]);
+    }
+
+    println!();
+    println!("Nota {}: {}", inteiro, notas[inteiro]);
+    println!();
 }
