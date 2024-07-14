f829b4590dabefb6e281ce48edfd37a9104d10d3
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-07-14 00:07:11 -0300

    Chapter 3: Functions with return values

--- rust/the_book/projects/functions/src/main.rs
+++ rust/the_book/projects/functions/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
 //    another_function(5);
-    printed_labeled_measurement(5, 'm');
+    printed_labeled_measurement(five(), 'm');
+
+    let x = plus_one(five());
+
+    println!("The value of x is: {x}");
 }
 
 fn another_function(x: i32) {
@@ -8,5 +12,13 @@ fn another_function(x: i32) {
 }
 
 fn printed_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+    println!("The measurement is: {value} {unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
