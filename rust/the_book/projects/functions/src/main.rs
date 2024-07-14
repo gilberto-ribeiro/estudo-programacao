13b959def0765323c18c69b866d59c0f0a06aca1
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-07-13 23:59:12 -0300

    Chapter 3: Functions with parameters

--- rust/the_book/projects/functions/src/main.rs
+++ rust/the_book/projects/functions/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+//    another_function(5);
+    printed_labeled_measurement(5, 'm');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn printed_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
