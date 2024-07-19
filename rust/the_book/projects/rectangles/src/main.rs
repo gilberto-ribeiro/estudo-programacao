9379f81206c61dcf25b2dcaf38f27923f9e317ed
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-07-19 00:36:10 -0300

    Chapter 5: Area of a rectangle, v3

--- rust/the_book/projects/rectangles/src/main.rs
+++ rust/the_book/projects/rectangles/src/main.rs
@@ -1,12 +1,21 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
+    println!("Width: {}. Height: {}.", rect1.width, rect1.height);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
