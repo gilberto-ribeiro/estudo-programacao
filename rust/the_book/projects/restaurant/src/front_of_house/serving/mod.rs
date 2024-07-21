8a026c719e66db7c77b7b2cc8dbe3684b4981444
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-07-21 17:13:20 -0300

    Chapter 7: Modules (Multiple files)

---
+++ rust/the_book/projects/restaurant/src/front_of_house/serving/mod.rs
@@ -0,0 +1,7 @@
+// Old style
+
+pub fn take_order() {}
+
+pub fn serve_order() {}
+
+fn take_payment() {}
--- rust/the_book/projects/restaurant/src/lib.rs
+++ rust/the_book/projects/restaurant/src/lib.rs
@@ -1,48 +1,7 @@
 const PI: f32 = 3.14159265358979324;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-
-        pub fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::customer::deliver_order();
-    }
-
-    fn cook_order() {}
-}
+mod front_of_house;
+mod back_of_house;
 
 use crate::front_of_house::hosting;
 
