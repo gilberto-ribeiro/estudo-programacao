a5c4313b52611058a804fac6249b95e5fdae65a1
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-10-12 13:11:28 +0000

    Chapter 2: Implementing parallel mandelbrot

--- rust/programming_rust/mandelbrot/src/main.rs
+++ rust/programming_rust/mandelbrot/src/main.rs
@@ -1,7 +1,6 @@
 use image::png::PNGEncoder;
 use image::ColorType;
 use num::Complex;
-use std::fmt::UpperExp;
 use std::fs::File;
 use std::str::FromStr;
 use std::env;
@@ -20,7 +19,28 @@ fn main() {
     let lower_right = parse_complex(&args[4])
         .expect("error parsing lower right corner point");
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+
+    // render(&mut pixels, bounds, upper_left, lower_right);
+    
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band*bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height =band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        }).unwrap();
+    }
+
     if let Err(e) = write_image(&args[1], &pixels, bounds) {
         eprintln!("Error: {e}");
         std::process::exit(1);
