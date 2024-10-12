2b2a9f302a51ed11474a8497a4e9fb40ff935b02
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-10-12 13:47:01 +0000

    Chapter 2: Mandelbrot using environment variables

--- rust/programming_rust/mandelbrot/src/main.rs
+++ rust/programming_rust/mandelbrot/src/main.rs
@@ -1,44 +1,46 @@
 use image::png::PNGEncoder;
 use image::ColorType;
 use num::Complex;
+use std::env;
 use std::fs::File;
 use std::str::FromStr;
-use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 5 {
         eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
-        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
         std::process::exit(1);
     }
-    let bounds = parse_pair(&args[2], 'x')
-        .expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[3])
-        .expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4])
-        .expect("error parsing lower right corner point");
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
     // render(&mut pixels, bounds, upper_left, lower_right);
-    
-    let threads = 8;
+
+    let threads = usize::from_str(&env::var("THREAD_NUMBER").unwrap_or("4".to_string())).unwrap();
     let rows_per_band = bounds.1 / threads + 1;
-    
+
     {
-        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band*bounds.0).collect();
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
         crossbeam::scope(|spawner| {
             for (i, band) in bands.into_iter().enumerate() {
                 let top = rows_per_band * i;
-                let height =band.len() / bounds.0;
+                let height = band.len() / bounds.0;
                 let band_bounds = (bounds.0, height);
                 let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
                 spawner.spawn(move |_| {
                     render(band, band_bounds, band_upper_left, band_lower_right);
                 });
             }
-        }).unwrap();
+        })
+        .unwrap();
     }
 
     if let Err(e) = write_image(&args[1], &pixels, bounds) {
